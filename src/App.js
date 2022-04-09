fb5e9a7184524b9d34dea7462d5894a6b64dd477
Alther Alves <para.alves@gmail.com>
2022-04-09 02:28:49 -0700

    open drawer by clicking Row table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'rsuite/dist/rsuite.min.css';
+import React from 'react';
+import { Container, Header, Content, Footer, Placeholder } from 'rsuite';
+import MyModal from './components/MyModal'
+import MyTable from './components/MyTable'
+import MyNavbar from './components/MyNavbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App show-fake-browser navbar-page">
+
+      <Container>
+        <Header>
+          <MyNavbar/>
+        </Header>
+        <Content>
+          <MyModal/>
+
+          <Placeholder/>
+          <Placeholder/>
+          <Placeholder/>
+          <Placeholder/>
+          
+          <MyTable/>
+        </Content>
+        <Footer></Footer>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
